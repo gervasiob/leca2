0bdfc585d9f22383ff5ea488f15048debc204516
Gervasio Hugo Barraco Marmol <gbarracomarmol@gmail.com>
2025-10-24 18:20:17 +0000

    inicialicemos el firebase
    // Import the functions you need from the SDK

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,23 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCtIyRpcWhMgqC__uCS5dxSpuvgWYc3h5U",
+  authDomain: "lecatex-ca042.firebaseapp.com",
+  projectId: "lecatex-ca042",
+  storageBucket: "lecatex-ca042.appspot.com",
+  messagingSenderId: "316458364732",
+  appId: "1:316458364732:web:87a9ea1893844418403839"
 };
 
-// Inicializa Firebase
-let app;
+// Initialize Firebase
+let app: FirebaseApp;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
 } else {
-  app = getApps()[0];
+  app = getApp();
 }
 
 const db = getFirestore(app);
